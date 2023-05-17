64572cd9409e3a41171eb9016fa23169aa70a15e
Mattia Magi <mattia.magi91@gmail.com>
2023-05-17 12:44:26 +0200

    added stories for every typography components

---
+++ stories/Text/Section.stories.tsx
@@ -0,0 +1,37 @@
+import { Section } from "../../components/Text";
+import { StoryFn, Meta } from '@storybook/react';
+
+export default {
+  title: "Typography Components/Section",
+  component: Section,
+  argTypes: {
+    divider: {
+      control: {
+        type: 'select',
+        options: ['none', 'top', 'bottom', 'both'],
+      },
+    },
+    display: {
+      control: {
+        type: 'select',
+        options: ['grid', 'flex'],
+      },
+    },
+    padding: {
+      control: {
+        type: 'select',
+        options: ['x', 'y', 'swimlane', 'all'],
+      },
+    },
+  },
+} as Meta;
+
+const SectionComponent: StoryFn = (args: {
+  divider?: 'none' | 'top' | 'bottom' | 'both';
+  display?: 'grid' | 'flex';
+  padding?: 'x' | 'y' | 'swimlane' | 'all';
+}) => <Section {...args}>This is the Section component</Section>
+
+export const DefaultSection = SectionComponent.bind({});
+
+DefaultSection.args = {};
--- stories/Text/Text.stories.tsx
+++ stories/Text/Text.stories.tsx
@@ -1,8 +1,8 @@
-import { Text, Heading, Section, PageHeader } from "../../components/Text";
+import { Text } from "../../components/Text";
 import { StoryFn, Meta } from '@storybook/react';
 
 export default {
-  title: "Text components",
+  title: "Typography Components/Text",
   component: Text,
   argTypes: {
     color: {
@@ -31,13 +31,7 @@ const TextComponent: StoryFn = (args: {
   size?: 'lead' | 'copy' | 'fine';
   width?: 'default' | 'narrow' | 'wide';
 }) => <Text {...args}>This is the text component</Text>
-// export const HeadingComponent = () => <Heading>This is the heading component</Heading>
-// export const SectionComponent = () => <Section heading={'heading'}>This is the section component</Section>
-// export const PageHeaderComponent = () => <PageHeader heading={'heading'}>This is the page header component</PageHeader>
-export const Default = TextComponent.bind({});
 
-Default.args = {
-  color: 'default',
-  size: 'lead',
-  width: 'default',
-};
+export const DefaultText = TextComponent.bind({});
+
+DefaultText.args = {};
