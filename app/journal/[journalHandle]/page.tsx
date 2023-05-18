910ad37c50c4bd2a438b309c483b153b36fc6076
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-05-18 18:15:19 +0300

    Add placeholder for /journal/:slug

---
+++ app/journal/[journalHandle]/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "@/components/Text";
+
+export default function JournalPage() {
+  return (
+    <>
+      <PageHeader heading="Blog post title" variant="blogPost">
+        <span>Date here &middot; auther name here</span>
+      </PageHeader>
+    </>
+  );
+}
