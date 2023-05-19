feef08786ccf81f2561304e2e0ded9da506e555e
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-05-19 18:13:26 +0300

    Fix name

--- app/journal/[journalHandle]/page.tsx
+++ app/journal/[journalHandle]/page.tsx
@@ -1,6 +1,6 @@
 import { PageHeader } from "@/components/Text";
 
-export default function JournalPage() {
+export default function JournalHandlePage() {
   return (
     <>
       <PageHeader heading="Blog post title" variant="blogPost">
