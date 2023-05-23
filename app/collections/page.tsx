e266246190c24c71a5624f74ef13faa8cf72bb7b
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-05-23 20:16:21 +0300

    Collections page

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,8 +1,86 @@
+import { Button } from "@/components/Button";
+import CollectionCard from "@/components/CollectionCard";
+import { Grid } from "@/components/Grid";
+import { PageHeader, Section } from "@/components/Text";
+import { Collection } from "@/lib/shopify/types";
+
 export default function Collections() {
-    return (
-      <>
-        HERE IS THE COLLECTIONS PAGE
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <PageHeader heading="Collections" />
+      <Section>
+        <Pagination connection={collections}>
+          {({
+            endCursor,
+            hasNextPage,
+            hasPreviousPage,
+            nextPageUrl,
+            nodes,
+            prevPageUrl,
+            startCursor,
+            nextLinkRef,
+            isLoading,
+          }) => (
+            <>
+              {hasPreviousPage && (
+                <div className="flex items-center justify-center mt-6">
+                  <Button
+                    to={prevPageUrl}
+                    variant="secondary"
+                    width="full"
+                    prefetch="intent"
+                    disabled={!isLoading}
+                    state={{
+                      pageInfo: {
+                        endCursor,
+                        hasNextPage,
+                        startCursor,
+                      },
+                      nodes,
+                    }}
+                  >
+                    {isLoading ? "Loading..." : "Previous products"}
+                  </Button>
+                </div>
+              )}
+              <Grid
+                items={nodes.length === 3 ? 3 : 2}
+                data-test="collection-grid"
+              >
+                {nodes.map((collection, i) => (
+                  <CollectionCard
+                    collection={collection as Collection}
+                    key={collection.id}
+                    loading={getImageLoadingPriority(i, 2)}
+                  />
+                ))}
+              </Grid>
+              {hasNextPage && (
+                <div className="flex items-center justify-center mt-6">
+                  <Button
+                    ref={nextLinkRef}
+                    to={nextPageUrl}
+                    variant="secondary"
+                    width="full"
+                    prefetch="intent"
+                    disabled={!isLoading}
+                    state={{
+                      pageInfo: {
+                        endCursor,
+                        hasPreviousPage,
+                        startCursor,
+                      },
+                      nodes,
+                    }}
+                  >
+                    {isLoading ? "Loading..." : "Next products"}
+                  </Button>
+                </div>
+              )}
+            </>
+          )}
+        </Pagination>
+      </Section>
+    </>
+  );
+}
