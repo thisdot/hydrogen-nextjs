faf48eb10c0f3e59a8d6180fa73f0faa396e131d
Jerry Hogan <jerry.hogan@thisdot.co>
2023-05-29 16:54:17 +0100

    Create radio form element

---
+++ components/stories/itemTabHeading.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import ItemTabHeading, { IItemTabHeading } from "../ItemTabHeading";
+
+export default {
+  title: "Components/ItemTabHeading",
+  component: ItemTabHeading,
+} as Meta;
+
+const SizeTemplate: StoryFn = () => (
+  <ItemTabHeading
+    options={[
+      { optionName: "Size", optionValue: "154cm" },
+      { optionName: "Size", optionValue: "158cm" },
+      { optionName: "Size", optionValue: "160cm" },
+    ]}
+  />
+);
+
+export const Size = SizeTemplate.bind({});
+
+const BindingTemplate: StoryFn = () => (
+  <ItemTabHeading
+    options={[
+      { optionName: "Binding mount", optionValue: "Nested" },
+      { optionName: "Binding mount", optionValue: "Optimistic" },
+    ]}
+  />
+);
+
+export const BindingMount = BindingTemplate.bind({});
