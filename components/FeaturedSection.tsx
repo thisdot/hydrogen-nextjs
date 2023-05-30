69560e5eadb3eafcc81240420d93ec6dcff0132f
Jerry Hogan <jerry.hogan@thisdot.co>
2023-05-30 18:13:05 +0100

    in progress

---
+++ components/FeaturedSection.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+// import { useFetcher } from "@remix-run/react";
+// import type {
+//   Collection,
+//   Product,
+// } from "@shopify/hydrogen/storefront-api-types";
+
+// import { usePrefixPathWithLocale } from "~/lib/utils";
+
+import { FeaturedCollections } from "./FeaturedCollection";
+import { ProductSwimlane } from "./ProductSwimlane";
+
+// export interface FeaturedData {
+//   featuredCollections: Collection[];
+//   featuredProducts: Product[];
+// }
+
+export function FeaturedSection() {
+  const { load, data } = useFetcher();
+  const path = usePrefixPathWithLocale("/featured-products");
+
+  useEffect(() => {
+    load(path);
+  }, [load, path]);
+
+  if (!data) return null;
+
+  const { featuredCollections, featuredProducts } = data as FeaturedData;
+
+  return (
+    <>
+      {featuredCollections.length < 2 && (
+        <FeaturedCollections
+          title="Popular Collections"
+          collections={featuredCollections}
+        />
+      )}
+      <ProductSwimlane products={featuredProducts} />
+    </>
+  );
+}
