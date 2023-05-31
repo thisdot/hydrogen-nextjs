a5bd9d2c74dce2e61ed3335471cbf23c95b00e4c
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2023-05-31 12:52:21 +0300

    Build out Products page (#84)
    * Build out collections page
    
    * Complete the pagination component
    
    * clean up
    
    * remove console log
    
    * fix hasNextPage
    
    * add const by page
    
    * add changelog
    
    * delete the old changelog

---
+++ lib/const.ts
@@ -0,0 +1,1 @@
+export const PAGE_BY = 8;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
-    domains: ['cdn.shopify.com']
-  }
-}
+    domains: ["cdn.shopify.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
