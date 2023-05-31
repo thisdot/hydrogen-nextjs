d7dc1bfce9b65f9c9db3a16127c7188fcc6593fa
Mark S. Shenouda <49324987+markshenouda@users.noreply.github.com>
2023-05-31 15:49:50 +0300

    Collections page (#83)
    * Collections page
    
    * add infinate loading to collections page
    
    * remove pagination
    
    * Update grid
    
    * remove data test
    
    * remove id
    
    * remove unused index

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,8 +1,32 @@
-export default function Collections() {
-    return (
-      <>
-        HERE IS THE COLLECTIONS PAGE
-      </>
-    )
-  }
-  
+import CollectionCard from "@/components/CollectionCard";
+import { Grid } from "@/components/Grid";
+import { PageHeader, Section } from "@/components/Text";
+import { PAGE_BY } from "@/lib/const";
+import { getAllCollections } from "@/lib/shopify";
+import LoadMoreCollections from "./components/LoadMoreCollections";
+
+export default async function Collections() {
+  const data = await getAllCollections({
+    variables: {
+      first: PAGE_BY,
+    },
+  });
+
+  return (
+    <>
+      <PageHeader heading="Collections" />
+      <Section>
+        <Grid items={data.body.data.collections.nodes.length === 3 ? 3 : 2}>
+          {data.body.data.collections.nodes.map((collection) => (
+            <CollectionCard collection={collection} key={collection.id} />
+          ))}
+        </Grid>
+        {data.body.data.collections.pageInfo.hasNextPage && (
+          <LoadMoreCollections
+            startCursor={data.body.data.collections.pageInfo.endCursor}
+          />
+        )}
+      </Section>
+    </>
+  );
+}
