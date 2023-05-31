74674782519b145390a4c62fc5fbfe933737273d
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-05-31 15:43:20 +0300

    remove id

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -17,7 +17,7 @@ export default async function Collections() {
       <PageHeader heading="Collections" />
       <Section>
         <Grid items={data.body.data.collections.nodes.length === 3 ? 3 : 2}>
-          {data.body.data.collections.nodes.map((collection, i) => (
+          {data.body.data.collections.nodes.map((collection) => (
             <CollectionCard collection={collection} key={collection.id} />
           ))}
         </Grid>
