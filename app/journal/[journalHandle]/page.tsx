20188ab115b9c9ce89b5a1246c7882c124f255c5
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-06-01 19:14:16 +0300

    feat: Build out individual journal entry page

--- app/journal/[journalHandle]/page.tsx
+++ app/journal/[journalHandle]/page.tsx
@@ -1,11 +1,32 @@
-import { PageHeader } from "@/components/Text";
+import { PageHeader, Section } from "@/components/Text";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Image from "next/image";
 
-export default function JournalHandlePage() {
+export default async function JournalHandlePage() {
   return (
     <>
       <PageHeader heading="Blog post title" variant="blogPost">
         <span>Date here &middot; auther name here</span>
       </PageHeader>
+
+      <Section as="article" padding="x">
+        {/* {image && (
+          <Image
+            src={image}
+            className="w-full mx-auto mt-8 md:mt-16 max-w-7xl"
+            sizes="90vw"
+            loading="eager"
+          />
+        )} */}
+        <div className="article">
+          {/* @ts-expect-error Server Component */}
+          <MDXRemote
+            source={`
+        # Hello, world!
+        `}
+          />
+        </div>
+      </Section>
     </>
   );
 }
