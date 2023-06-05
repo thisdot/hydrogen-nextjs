298a2aecbb1db10ae2ba0586b082f438ca430cd7
Mattia Magi <mattia.magi91@gmail.com>
2023-06-05 18:46:23 +0200

    clean code

--- app/collections/[collectionHandle]/page.tsx
+++ app/collections/[collectionHandle]/page.tsx
@@ -1,7 +1,7 @@
 import { PageHeader, Text, Section } from '@/components/Text';
 import ProductGrid from '@/components/ProductGrid';
 import { Collection, Filter } from '@/lib/shopify/types';
-import { AppliedFilter, SortFilter, SortParam } from '@/components/SortFilter';
+import { SortFilter } from '@/components/SortFilter';
 import { getCollectionProducts } from '@/lib/shopify';
 import { flattenConnection } from '@/lib/flattenConnection';
 import { handleCollectionProductsSearchParams } from '@/lib/handleCollectionProductsSearchParams';
@@ -28,59 +28,6 @@ export default async function Collection({ params, searchParams }: { params: { c
     filters,
     appliedFilters } = handleCollectionProductsSearchParams(searchParams)
 
-  // const variantOption = 'variantOption';
-  // const { sortKey, reverse } = getSortValuesFromParam(
-  //   urlSearchParams.get('sort') as SortParam,
-  // );
-  // const cursor = urlSearchParams.get('cursor');
-  // const knownFilters = ['productVendor', 'productType'];
-  // const available = 'available';
-
-  // const filters: FiltersQueryParams = [];
-  // const appliedFilters: AppliedFilter[] = [];
-
-  // for (const [key, value] of Array.from(urlSearchParams.entries())) {
-  //   if (available === key) {
-  //     filters.push({ available: value === 'true' });
-  //     appliedFilters.push({
-  //       label: value === 'true' ? 'In stock' : 'Out of stock',
-  //       urlParam: {
-  //         key: available,
-  //         value,
-  //       },
-  //     });
-  //   } else if (knownFilters.includes(key)) {
-  //     filters.push({ [key]: value });
-  //     appliedFilters.push({ label: value, urlParam: { key, value } });
-  //   } else if (key.includes(variantOption)) {
-  //     const [name, val] = value.split(':');
-  //     filters.push({ variantOption: { name, value: val } });
-  //     appliedFilters.push({ label: val, urlParam: { key, value } });
-  //   }
-  // }
-  // // Builds min and max price filter since we can't stack them separately into
-  // // the filters array. See price filters limitations:
-  // // https://shopify.dev/custom-storefronts/products-collections/filter-products#limitations
-  // if (urlSearchParams.has('minPrice') || urlSearchParams.has('maxPrice')) {
-  //   const price: { min?: number; max?: number } = {};
-  //   if (urlSearchParams.has('minPrice')) {
-  //     price.min = Number(urlSearchParams.get('minPrice')) || 0;
-  //     appliedFilters.push({
-  //       label: `Min: $${price.min}`,
-  //       urlParam: { key: 'minPrice', value: urlSearchParams.get('minPrice')! },
-  //     });
-  //   }
-  //   if (urlSearchParams.has('maxPrice')) {
-  //     price.max = Number(urlSearchParams.get('maxPrice')) || 0;
-  //     appliedFilters.push({
-  //       label: `Max: $${price.max}`,
-  //       urlParam: { key: 'maxPrice', value: urlSearchParams.get('maxPrice')! },
-  //     });
-  //   }
-  //   filters.push({
-  //     price,
-  //   });
-  // }
 
   const data = await getCollectionProducts(
     {
@@ -127,42 +74,3 @@ export default async function Collection({ params, searchParams }: { params: { c
     </>
   );
 }
-
-
-
-// function getSortValuesFromParam(sortParam: SortParam | null) {
-//   switch (sortParam) {
-//     case 'price-high-low':
-//       return {
-//         sortKey: 'PRICE',
-//         reverse: true,
-//       };
-//     case 'price-low-high':
-//       return {
-//         sortKey: 'PRICE',
-//         reverse: false,
-//       };
-//     case 'best-selling':
-//       return {
-//         sortKey: 'BEST_SELLING',
-//         reverse: false,
-//       };
-//     case 'newest':
-//       return {
-//         sortKey: 'CREATED',
-//         reverse: true,
-//       };
-//     case 'featured':
-//       return {
-//         sortKey: 'MANUAL',
-//         reverse: false,
-//       };
-//     default:
-//       return {
-//         sortKey: 'RELEVANCE',
-//         reverse: false,
-//       };
-//   }
-// }
-
-
