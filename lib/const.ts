99f39bd8259459979351ff9dbcef89c8516dd837
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2023-06-06 23:15:50 +0300

    feat: Create useAnalytics hook

--- lib/const.ts
+++ lib/const.ts
@@ -1,2 +1,28 @@
 export const PAGE_BY = 8;
 export const BLOG_HANDLE = "Journal";
+export const SHOPIFY_Y = "_shopify_y";
+export const SHOPIFY_S = "_shopify_s";
+
+export const ShopifySalesChannel: ShopifySalesChannel = {
+  hydrogen: "hydrogen",
+  headless: "headless",
+} as const;
+
+export interface ShopifySalesChannel {
+  /** Shopify Hydrogen sales channel */
+  hydrogen: "hydrogen";
+  /** Shopify Headless sales channel */
+  headless: "headless";
+}
+
+export const AnalyticsEventName: AnalyticsEventName = {
+  PAGE_VIEW: "PAGE_VIEW",
+  ADD_TO_CART: "ADD_TO_CART",
+} as const;
+
+interface AnalyticsEventName {
+  /** Page view */
+  PAGE_VIEW: "PAGE_VIEW";
+  /** Add to cart */
+  ADD_TO_CART: "ADD_TO_CART";
+}
