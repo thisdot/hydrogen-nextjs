6d8165973db2ced794f475cac6f97fda542f74a6
Jerry Hogan <jerry.hogan@thisdot.co>
2023-06-14 19:28:00 +0100

    changed route path

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import isAuthenticated from './lib/isAuthenticated';
 
 export const config = {
-	matcher: ['/checkout', '/account', '/account/login', '/account/register', '/account/forgot-password']
+	matcher: ['/checkout', '/account', '/account/login', '/account/register', '/account/recover']
 };
 
 export function middleware(request: NextRequest) {
 	const isLoginPage = request.nextUrl.pathname.startsWith('/account/login');
-	const isPasswordPage = request.nextUrl.pathname.startsWith(
-		'/account/forgot-password'
+	const isRecoverPasswordPage = request.nextUrl.pathname.startsWith(
+		'/account/recover'
 	);
 	const isRegisterPage =
 		request.nextUrl.pathname.startsWith('/account/register');
 
-		if((isLoginPage || isPasswordPage || isRegisterPage) && isAuthenticated(request)) {
+		if((isLoginPage || isRecoverPasswordPage || isRegisterPage) && isAuthenticated(request)) {
 			return NextResponse.redirect(new URL('/account', request.url));
 		}
 
-		if (!(isLoginPage || isPasswordPage || isRegisterPage) && !isAuthenticated(request)) {
+		if (!(isLoginPage || isRecoverPasswordPage || isRegisterPage) && !isAuthenticated(request)) {
 			return NextResponse.redirect(new URL('/account/login', request.url));
 		}
 }
