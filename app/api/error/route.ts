f80ed324074ce2411da324bd184f0cebfa79375d
Mattia Magi <mattia.magi91@gmail.com>
2023-07-14 11:46:48 +0200

    format

--- app/api/error/route.ts
+++ app/api/error/route.ts
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 import { getFeaturedProducts, getFeaturedCollections } from '@/lib/shopify';
 import { NextResponse } from 'next/server';
 
