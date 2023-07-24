1d82669b94a53dd4b857b162b5481b4013f6e267
Jerry Hogan <jerry.hogan@thisdot.co>
2023-07-24 15:31:14 +0100

    Policies

---
+++ lib/shopify/queries/policies.ts
@@ -0,0 +1,62 @@
+export const POLICIES_QUERY = `#graphql
+  fragment PolicyIndex on ShopPolicy {
+    id
+    title
+    handle
+  }
+
+  query PoliciesIndex {
+    shop {
+      privacyPolicy {
+        ...PolicyIndex
+      }
+      shippingPolicy {
+        ...PolicyIndex
+      }
+      termsOfService {
+        ...PolicyIndex
+      }
+      refundPolicy {
+        ...PolicyIndex
+      }
+      subscriptionPolicy {
+        id
+        title
+        handle
+      }
+    }
+  }
+`;
+
+export const POLICY_CONTENT_QUERY = `#graphql
+  fragment PolicyHandle on ShopPolicy {
+    body
+    handle
+    id
+    title
+    url
+  }
+
+  query PoliciesHandle(
+    $language: LanguageCode
+    $privacyPolicy: Boolean!
+    $shippingPolicy: Boolean!
+    $termsOfService: Boolean!
+    $refundPolicy: Boolean!
+  ) @inContext(language: $language) {
+    shop {
+      privacyPolicy @include(if: $privacyPolicy) {
+        ...PolicyHandle
+      }
+      shippingPolicy @include(if: $shippingPolicy) {
+        ...PolicyHandle
+      }
+      termsOfService @include(if: $termsOfService) {
+        ...PolicyHandle
+      }
+      refundPolicy @include(if: $refundPolicy) {
+        ...PolicyHandle
+      }
+    }
+  }
+`;
