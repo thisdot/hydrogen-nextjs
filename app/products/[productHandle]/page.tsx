79353932ec14d450d023597ad04b35e3ca98e924
Mattia Magi <mattia.magi91@gmail.com>
2023-09-15 17:43:37 +0200

    feat: product page little ui changes

--- app/products/[productHandle]/page.tsx
+++ app/products/[productHandle]/page.tsx
@@ -133,10 +133,10 @@ export default async function Product({
 				<script type="application/ld+json">{JSON.stringify(seo.jsonLd)}</script>
 			</Head>
 			<Section className="px-0 md:px-8 lg:px-12">
-				<div className="grid items-start md:gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+				<div className="grid items-start md:gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-2">
 					<ProductGallery
 						media={media.nodes}
-						className="w-full lg:col-span-2"
+						className="w-full lg:col-span-1"
 					/>
 					<div className="sticky md:-mb-nav md:top-nav md:-translate-y-nav md:h-screen md:pt-nav hiddenScroll md:overflow-y-scroll">
 						<section className="flex flex-col w-full max-w-xl gap-8 p-6 md:mx-auto md:max-w-sm md:px-0">
